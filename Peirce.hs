2ef8fe394bbb9a37aafddb60b09c40e25a5c1bcc
Neelu Saraswatibhatla <neelu@sbneelu.com>
2022-02-13 16:30:02 +0000

    Formatting

--- Peirce.hs
+++ Peirce.hs
@@ -3,8 +3,8 @@ module Peirce where
 import Prover
 
 main :: IO ()
-main = 
-    let {p = Atom "P"; q = Atom "Q"} in
-        let prop = Implies(Implies(Implies(p, q), p), p) in
-            let proof = prove prop in
-                print (proofToJson proof)
+main =
+  let p = Atom "P"; q = Atom "Q"
+   in let prop = Implies (Implies (Implies (p, q), p), p)
+       in let proof = prove prop
+           in print (proofToJson proof)
